942b320412e71dfd68ab9180be6a1d937390a374
Stalgis <118928297+Stalgis@users.noreply.github.com>
2025-08-13 20:57:01 +1000

    Revert "feat(users): add DTOs and email uniqueness check"

--- classProject/server/src/modules/users/users.service.ts
+++ classProject/server/src/modules/users/users.service.ts
@@ -1,21 +1,18 @@
-import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './users.schema';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const existingUser = await this.userModel
-      .findOne({ email: createUserDto.email })
-      .exec();
-    if (existingUser) {
-      throw new ConflictException('Email already exists');
-    }
+  async create(createUserDto: any): Promise<User> {
     const user = new this.userModel(createUserDto);
     return user.save();
   }
@@ -36,7 +33,7 @@ export class UsersService {
     return this.userModel.findOne({ email }).exec();
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+  async update(id: string, updateUserDto: any): Promise<User> {
     const updatedUser = await this.userModel
       .findByIdAndUpdate(id, updateUserDto, { new: true })
       .exec();
